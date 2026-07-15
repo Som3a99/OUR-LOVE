470926606703118a8cea26d9461b2ba690cdf14d
Mohamed Ismail <Mohamed_EMohamed@outlook.com>
2026-07-15 19:35:41 +0300

    Scene02

--- js/audio.js
+++ js/audio.js
@@ -89,3 +89,82 @@ const AudioManager = (function() {
     crossfadeTo
   };
 })();
+
+const AudioManager = (function() {
+  let currentMusic = null;
+  let currentId = null;
+
+  function play(soundKey, volume = 0.18, fadeIn = 3000) {
+    stop();
+    try {
+      const sound = new Howl({
+        src: [`assets/audio/${soundKey}.mp3`],
+        volume: 0,
+        loop: true,
+        preload: true,
+      });
+      currentId = sound.play();
+      sound.fade(0, volume, fadeIn, currentId);
+      currentMusic = sound;
+    } catch(e) {}
+  }
+
+  function playSnippet(soundKey, start, duration, volume = 0.18, fadeIn = 2500) {
+    stop();
+    try {
+      const sound = new Howl({
+        src: [`assets/audio/${soundKey}.mp3`],
+        sprite: { snippet: [start, duration, false] }, // لا تكرار
+        volume: 0,
+        preload: true,
+      });
+      currentId = sound.play('snippet');
+      sound.fade(0, volume, fadeIn, currentId);
+      currentMusic = sound;
+    } catch(e) {}
+  }
+
+  // المقطع الواحد الطويل من 0:18 إلى 2:45 (147 ثانية)
+  function playThreshold() {
+    playSnippet('threshold', 18000, 147000, 0.18, 2500);
+  }
+
+  function stop() {
+    if (currentMusic) {
+      currentMusic.stop();
+      currentMusic = null;
+    }
+  }
+
+  function fadeOut(duration = 2000) {
+    if (currentMusic) {
+      currentMusic.fade(currentMusic.volume(), 0, duration);
+      setTimeout(() => stop(), duration);
+    }
+  }
+
+  function crossfadeTo(newSoundKey, newVolume = 0.15, duration = 4000) {
+    if (!currentMusic) {
+      play(newSoundKey, newVolume, duration);
+      return;
+    }
+    const old = currentMusic;
+    old.fade(old.volume(), 0, duration);
+    setTimeout(() => old.stop(), duration + 200);
+
+    try {
+      const sound = new Howl({
+        src: [`assets/audio/${newSoundKey}.mp3`],
+        volume: 0,
+        loop: true,
+        preload: true,
+      });
+      const id = sound.play();
+      sound.fade(0, newVolume, duration, id);
+      currentMusic = sound;
+      currentId = id;
+    } catch(e) {}
+  }
+
+  return { play, playSnippet, playThreshold, stop, fadeOut, crossfadeTo };
+})();
